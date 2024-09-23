4aba715157ff5cc2dab135204d0df1c9b8cb4d6e
janvihatwar <janvihatwar7@gmail.com>
2024-09-24 00:43:47 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -2,44 +2,47 @@ import './App.css';
 import Body from './components/Body';
 import Navbar from './components/Navbar';
 import Alert from './components/Alert'
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  const[mode,setMode]=useState('light');
-  const[alert,setAlert]=useState(null);  {/*alert is earlier set as null */}
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
 
-  const showAlert=(message,type)=>{   {/* will take iinput of message and type of alert*/}
+  const showAlert = (message, type) => {
     setAlert({
-      msg:message,
-      type:type
+      msg: message,
+      type: type
     })
     setTimeout(() => {
       setAlert(null);
     }, 1400);
-  
   }
-  const toggleMode=()=>{
-    if(mode==='light') 
-    {
-      setMode('dark');
-      document.body.style.backgroundColor='grey';
-      showAlert("Dark mode has been enabled!!","success");   {/*passing msg and type to the function */}
-      // document.title="TextAnalyzer-Dark Mode";           {/*for changing the doument title according to title */}
-    }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor='white';
-      showAlert("Light mode has been enabled!!","success");
-      // document.title="TextAnalyzer-Light Mode";        
-    }
 
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+      showAlert("Dark mode has been enabled!!", "success");
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled!!", "success");
+    }
   }
+
   return (
     <Router>
-      <Routes>
-        {/* Your routes here */}
-      </Routes>
+      <div className="App">
+        <Navbar mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route path="/" element={<Body mode={mode} showAlert={showAlert} />} />
+            {/* Add more routes here if needed */}
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 }
